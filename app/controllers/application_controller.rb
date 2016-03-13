fcc4820676a32f4313dd825aebf9c6ca8ac4ecda
Robert O'Connor <rob@oconnor.ninja>
2016-03-12 22:26:09 -0500

    For now, do not export JsonApi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,12 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
   protected
+  def default_serializer_options
+    { serializer: UserSerializer } 
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
