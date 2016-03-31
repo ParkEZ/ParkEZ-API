2339c3858fa1bde769e1610dfa5cc32a84a4afd1
Robert O'Connor <rob@oconnor.ninja>
2016-03-31 00:27:18 -0400

    Add schema dump, add model specs

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it 'has a valid factory' do
+    expect(FactoryGirl.create(:user)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    user = User.create(email: 'rob@foo.com', name: nil)
+    expect(user).to_not be_valid
+  end
+
+  it 'is invalid without an email' do
+    user = User.create(email: nil, name: 'John Doe')
+    expect(user).to_not be_valid
+  end
+end
