8d45bede1cee4e4a50b7a6fc355812f3aa0eaa51
Robert O'Connor <rob@oconnor.ninja>
2016-04-07 19:57:44 -0400

    Make it one to one – a user can only check in to one spot

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
-  has_many :parking_locations
+  has_one :parking_locations
   validates :name, presence: true
 end
